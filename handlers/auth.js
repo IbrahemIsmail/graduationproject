f61c5926844d327966d010b986889ce1f4bd4f04
IbrahemIsmail <ibraxisma878@gmail.com>
2022-01-26 01:20:44 +0300

    added logout?

--- handlers/auth.js
+++ handlers/auth.js
@@ -60,5 +60,5 @@ module.exports = (passport) => {
         });
     }));
 
-    connection.end()
+    // connection.end()
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,24 +2,33 @@ const express = require('express');
 var passport = require('passport');
 const router = express.Router();
 
-router.get('/signup', (req, res)=>{
-    
+router.get('/signup', (req, res) => {
+
 });
 
-router.get('/login', (req, res)=>{
-    
+router.get('/login', (req, res) => {
+
 });
 
 router.post('/signup', passport.authenticate('signup', {
     successRedirect: '/home',
-    failureRedirect: '/signup',
+    failureRedirect: '/auth/signup',
     failureFlash: true
 }));
 
 router.post('/login', passport.authenticate('login', {
     successRedirect: '/home',
-    failureRedirect: '/login',
+    failureRedirect: '/auth/login',
     failureFlash: true
-}));
+}), (req, res) => {
+    console.log('hello?');
+    res.redirect('/');
+});
+
+
+router.get('/logout', (req, res) =>{
+    req.logout();
+    res.redirect('/');
+});
 
 module.exports = router;
