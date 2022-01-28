16464403fbacedb5e7138fd2f58f59245958dbfd
IbrahemIsmail <ibraxisma878@gmail.com>
2022-01-28 06:19:31 +0300

    fixed user page

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,7 @@
 const middlewareObj = {};
 
 middlewareObj.authUser = (req, res, next) => {
-    if(req.isAuthenticated() && req.currUser.username == req.params.username) return next();
+    if(req.isAuthenticated() && req.user.username == req.params.username) return next();
     else{
         req.flash('error', 'You Don\'t Have Permission To Do That');
         res.redirect("back");
