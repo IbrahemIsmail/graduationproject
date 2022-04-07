e9b61ed5acd0ea77fdf4e2f3ba724348f1b6d008
Ibrahem Ismail <ibraxisma878@gmail.com>
2022-04-07 10:58:58 +0300

    refactored middleware queries to use async await with mysql2

--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 const db = require('../models/database');
 
 
-pool = mysql.createPool(db.conn);
-pool.getConnection(async (err, connection) => {
+const pool = mysql.createPool(db.conn);
+const promisePool = pool.promise();
+
+promisePool.getConnection(async (err, connection) => {
     if (err) throw err;
     try {
         await connection.query('USE ' + db.conn.database);
@@ -35,30 +37,24 @@ middlewareObj.isLoggedIn = (req, res, next) => {
 }
 
 
-middlewareObj.authUserPost = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        pool.getConnection((err, connection) => {
-            if (err) throw err;
-            try {
-                connection.query(`SELECT * FROM posts JOIN postownership on posts.id = postownership.postID where postownership.studentID = ${req.user.id} AND posts.id = ${req.params.id}`, (err, rows) => {
-                    if (err) throw err;
-                    if (!rows.length) {
-                        console.log('You Don\'t Have Permission To Do That');
-                        req.flash('error', 'You Don\'t Have Permission To Do That');
-                        res.redirect("back");
-                    }
-                    else return next();
-                });
-            } catch (err) {
-                throw err;
-            } finally {
-                connection.release();
+middlewareObj.authUserPost = async (req, res, next) => {
+    try {
+        if (req.isAuthenticated()) {
+
+            let rows = await promisePool.query(`SELECT * FROM posts JOIN postownership on posts.id = postownership.postID where postownership.studentID = ${req.user.id} AND posts.id = ${req.params.id}`);
+            if (!rows[0].length) {
+                console.log('You Don\'t Have Permission To Do That');
+                req.flash('error', 'You Don\'t Have Permission To Do That');
+                res.redirect("back");
             }
-        });
-    }
-    else {
-        req.flash("error", "Please Log In First");
-        res.redirect("/login");
+            else return next();
+        }
+        else {
+            req.flash("error", "Please Log In First");
+            res.redirect("/login");
+        }
+    } catch (err) {
+        throw err;
     }
 }
 
