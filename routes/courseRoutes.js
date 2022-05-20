334852029c624a9fa27a5bf4249c053508f93620
RagheedObeid <ragheedobaid@gmail.com>
2022-05-21 00:13:51 +0300

    temp

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,15 +1,5 @@
 const express = require('express');
 const mw = require('../middleware')
-<<<<<<< Updated upstream
-const {searchCourseInstance, getCourse, getCourses, createCourse, createTeacher, createCourseInstance} = require('../handlers/courses');
-const router = express.Router();
-
-router.get('/', getCourses);
-router.get('/id=:id', getCourse);
-
-<<<<<<< HEAD
-router.post('/searchData', searchCourseInstance);
-=======
 const {createCourse, createTeacher, createCourseInstance, showforums} = require('../handlers/courses');
 const router = express.Router();
 
@@ -17,10 +7,6 @@ const router = express.Router();
 router.get('/addcourse', mw.authAdminUser, showforums);
 
 
->>>>>>> Stashed changes
-=======
-router.post('/searchData', searchCourseInstance); // this route looks stupid, change it
->>>>>>> @{u}
 router.post('/addcourse', mw.authAdminUser, createCourse);
 router.post('/addteacher', mw.authAdminUser, createTeacher);
 router.post('/addcourseinstance', mw.authAdminUser, createCourseInstance);
