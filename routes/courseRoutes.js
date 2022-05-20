57f206c4d3862e7a3df3ba475272831da317d909
IbrahemIsmail <ibraxisma878@gmail.com>
2022-05-20 21:13:23 +0300

    added comments about course routes

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', getCourses);
 router.get('/id=:id', getCourse);
 
-router.post('/searchData', searchCourseInstance);
+router.post('/searchData', searchCourseInstance); // this route looks stupid, change it
 router.post('/addcourse', mw.authAdminUser, createCourse);
 router.post('/addteacher', mw.authAdminUser, createTeacher);
 router.post('/addcourseinstance', mw.authAdminUser, createCourseInstance);
