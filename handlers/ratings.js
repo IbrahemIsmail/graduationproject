528196fa7c37605f18414269e22900a627777368
IbrahemIsmail <ibraxisma878@gmail.com>
2022-05-21 22:53:17 +0300

    added upvote or downvote functionality to ratings

--- handlers/ratings.js
+++ handlers/ratings.js
@@ -18,22 +18,28 @@ promisePool.getConnection(async (err, connection) => {
     }
 });
 
-checkDouble = (num)=>{
-    if(num >= 0 && num <=5) return num;
-    else if(num < 0) return 0;
-    else if(num > 5) return 5;
+checkDouble = (num) => {
+    if (num >= 0 && num <= 5) return num;
+    else if (num < 0) return 0;
+    else if (num > 5) return 5;
 }
 
-exports.giveRating = async (req, res) =>{
+checkVote = (num) => {
+    if (num == -1 || num == 1 || num == 0) return num;
+    else if (num < -1) return -1;
+    else if (num > 1) return 1;
+}
+
+exports.giveRating = async (req, res) => {
     try {
         userID = await promisePool.query(`SELECT id FROM users WHERE username = '${req.user.username}'`);
         let reviews = await promisePool.query(`SELECT * FROM ratings WHERE courseInstanceID = ${req.params.id}`);
         reviews[0].forEach((review) => {
-            if(review.userID == userID[0][0].id) {
+            if (review.userID == userID[0][0].id) {
                 throw Error("You can only give one review");
             }
         });
-         rating = {
+        rating = {
             userID: userID[0][0].id,
             courseID: parseInt(req.params.id),
             curriculum: checkDouble(req.body.curriculum),
@@ -46,6 +52,44 @@ exports.giveRating = async (req, res) =>{
         }
         let query = "INSERT INTO ratings (userID, courseInstanceID, curriculum, teacher, teachingMethods, expectations, exams, difficulty, description) VALUES (?,?,?,?,?,?,?,?,?)";
         await promisePool.query(query, [rating.userID, rating.courseID, rating.curriculum, rating.teacher, rating.teachingMethods, rating.expectations, rating.exams, rating.difficulty, rating.description]);
+    } catch (err) {
+        console.log(err);
+        req.flash('error', err.message || 'Oops! something went wrong.');
+        res.redirect('back');
+        return;
+    }
+}
+
+//add another table since this's a many to many relationship, figure out the rest of this function
+
+exports.upOrDownVote = async (req, res) => {
+    try {
+        userID = await promisePool.query(`SELECT id FROM users WHERE username = '${req.user.username}'`);
+        let voteNum = checkVote(req.body.vote);
+        let checkData = await promisePool.query(`select v.id as voteID, v.userID, r.id, v.vote from votes v left join voteownership vo on v.id = vo.voteID left join ratings r on r.id = vo.ratingID WHERE (v.userID = ${userID[0][0].id} And r.id = ${req.params.id})`);
+
+        if(checkData[0][0].userID == userID[0][0].id && checkData[0][0].id == req.params.id){
+            if(checkData[0][0].vote == voteNum){
+                await promisePool.query(`UPDATE votes SET vote = 0 WHERE id = ${checkData[0][0].voteID}`);
+            }
+            else if (checkData[0][0].vote == 1 && voteNum == -1){
+                await promisePool.query(`UPDATE votes SET vote = -1 WHERE id = ${checkData[0][0].voteID}`);
+            }
+            else if (checkData[0][0].vote == -1 && voteNum == 1){
+                await promisePool.query(`UPDATE votes SET vote = 1 WHERE id = ${checkData[0][0].voteID}`);
+            }
+            else if (checkData[0][0].vote == 0){
+                await promisePool.query(`UPDATE votes SET vote = ${voteNum} WHERE id = ${checkData[0][0].voteID}`);
+            }
+            return;
+        }
+
+        let voteQuery = 'INSERT INTO votes (userID, vote) VALUES (?,?)';
+        let vote = await promisePool.query(voteQuery, [userID[0][0].id, voteNum]);
+
+        let voteOwnershipQuery = "INSERT INTO voteownership (ratingID, voteID) VALUES (?,?)";
+        await promisePool.query(voteOwnershipQuery, [req.params.id, vote[0].insertId]);
+        res.redirect('back');
     } catch (err) {
         console.log(err);
         req.flash('error', err.message || 'Oops! something went wrong.');
